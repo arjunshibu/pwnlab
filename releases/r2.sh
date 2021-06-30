b44e58205fc0283a501055c2c73192db87eb5f7b
arjunshibu <arjunshibu1999@gmail.com>
2021-06-30 10:57:33 +0530

    update

--- releases/r2.sh
+++ releases/r2.sh
@@ -3,6 +3,6 @@
 latest=$(curl -s https://github.com/radareorg/radare2/releases/latest | grep 'href' | cut -d'"' -f2)
 release=$(curl -s $latest | grep -E 'href.*radare2_.*_amd64\.deb' | cut -d'"' -f2)
 
-wget -q --show-progress https://github.com/$release -O r2.deb
+wget -q https://github.com/$release -O r2.deb
 dpkg -i r2.deb
 rm r2.deb
